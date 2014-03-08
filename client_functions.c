4523be51dd38828a2c1d3b6ea63e874dd86a6f31
Linus Järvelä <linus.jarvela@gmail.com>
2014-03-08 17:23:50 +0100

    Renamed client .h and .c files.

---
+++ client_functions.c
@@ -0,0 +1,21 @@
+#include "protocol_std.h"
+#include "client_functions.h"
+
+void prepareSocket(int* sock_fd, struct sockaddr_in* sockaddr) {
+    printf("Preparing socket... ");
+    if((*sock_fd=socket(AF_INET, SOCK_DGRAM, 0))<0){
+        perror("Could not create socket.");
+        return;
+    }
+
+    memset((char*)sockaddr, 0, sizeof(*sockaddr));
+    sockaddr->sin_family = AF_INET;
+    sockaddr->sin_addr.s_addr = htonl(INADDR_ANY);
+    sockaddr->sin_port = htons(PORT);
+
+    if (bind(*sock_fd, (struct sockaddr*)sockaddr, sizeof(*sockaddr)) < 0) {
+        perror("Bind failed");
+        return;
+    }
+    printf("Socket prepared!\n");
+}
---
+++ client_functions.h
@@ -0,0 +1,7 @@
+#ifndef CLIENT_FUNCTIONS_H
+#define CLIENT_FUNCTIONS_H
+
+void prepareSocket(int* sock_fd, struct sockaddr_in* sockaddr);
+
+
+#endif //CLIENT_FUNCTIONS_H
