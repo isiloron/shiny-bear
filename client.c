e5438c92210f38e31bcb1093f70c9d8167e9e836
Linus Järvelä <linus.jarvela@gmail.com>
2014-03-09 19:17:33 +0100

    Client can reach established state

--- client.c
+++ client.c
@@ -66,13 +66,55 @@ int main(int argc, char *argv[])
                     if(receivedFrame->flags == SYN+ACK)
                     {
                         printf("SYN+ACK received!\n");
+                        frameToSend = newFrame(ACK,0,0,0);
+                        sendFrame(sfd, frameToSend, servAddr);
+                        free(frameToSend);
+                        state = PRECAUTION;
+                        break;
                     }
+                    free(receivedFrame);
                 }
             }
+            if(state==PRECAUTION)
+            {
+                break;
+            }
+            printf("Long timeout! State CLOSED.\n");
+            state = CLOSED;
             break;
         case PRECAUTION:
+            for(numOfShortTimeouts=0; numOfShortTimeouts<longTimeOut; numOfShortTimeouts++)
+            {
+                resetShortTimeout(&shortTimeout);
+                if(waitForFrame(sfd,&shortTimeout) == 0)
+                {
+                    state = ESTABLISHED;
+                    break;
+                }
+                else
+                {
+                    receivedFrame = receiveFrame(sfd, &servAddr);
+                    if(receivedFrame->flags == SYN+ACK)
+                    {
+                        printf("SYN+ACK received!\n");
+                        frameToSend = newFrame(ACK,0,0,0);
+                        sendFrame(sfd, frameToSend, servAddr);
+                        free(frameToSend);
+                        printf("Resent ACK.\n");
+                    }
+                    free(receivedFrame);
+                }
+            }
+            if(state==ESTABLISHED)
+            {
+                break;
+            }
+            printf("Long timeout! State CLOSED.\n");
+            state = CLOSED;
             break;
         case ESTABLISHED:
+            while(1)
+                printf("ESTBLISHED STATE REACHED!\n");
             break;
         case INIT_TEARDOWN:
             break;
