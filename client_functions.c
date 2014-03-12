a274b916e6777a128994bfd52f85b127665eba8e
Linus Järvelä <linus.jarvela@gmail.com>
2014-03-12 09:46:37 +0100

    Further client sliding window development

--- client_functions.c
+++ client_functions.c
@@ -22,10 +22,28 @@ void prepareHostAddr(struct sockaddr_in* servAddr, char* hostName, int port)
 int clientSlidingWindow(int sfd, struct sockaddr_in* servAddr)
 {
     rtp* frameToSend;
-    rtp* receivedFrame;
+    //rtp* receivedFrame;
     char messageString[MAXMSG];
-    struct Buffer* buffer;
+    /*struct Buffer* buffer;
+    struct timeval shortTimeout;
+    int numOfShortTimeouts = 0;
 
+    struct WindowStruct
+    {
+        rtp* frameSeq[MAXSEQ];
+        int startSeq;
+        int endSeq;
+    };
+
+    struct WindowStruct window;
+    window.startSeq = 0;
+    window.endSeq = 0;*/
+
+    //tråd som tar in ett meddelande från användaren och skickar iväg frames
+
+    //select loop med timeout som läser ackar.
+
+    int seq = 0;
     while(1)
     {
         fflush(stdin);
@@ -36,17 +54,37 @@ int clientSlidingWindow(int sfd, struct sockaddr_in* servAddr)
         }
         else
         {
+            printf("Sending INF.\n");
+            frameToSend = newFrame(INF,seq,0,strlen(messageString));
+            sendFrame(sfd, frameToSend, *servAddr);
+            seq=(seq+1)%MAXSEQ;
+            free(frameToSend);
+            int i;
+            for(i=0;i<strlen(messageString);i++)
+            {
+                frameToSend = newFrame(ACK,seq,0,messageString[i]);
+                sendFrame(sfd, frameToSend, *servAddr);
+                seq=(seq+1)%MAXSEQ;
+                free(frameToSend);
+            }
+            frameToSend = newFrame(ACK,seq,0,'\0');
+            sendFrame(sfd, frameToSend, *servAddr);
+            seq=(seq+1)%MAXSEQ;
+            free(frameToSend);
+
+            /*
             printf("Sending 'A'\n");
-            frameToSend = newFrame(2,1,2,3);
+            frameToSend = newFrame(ACK,10,-7,messageString[0]);
             buffer = newBuffer();
             serializeFrame(frameToSend,buffer);
             buffer->next = 0;
             receivedFrame = newFrame(0,0,0,0);
             deserializeFrame(receivedFrame,buffer);
-            printf("%d %d %d %d\n",frameToSend->flags,frameToSend->seq,frameToSend->crc,frameToSend->data);
-            printf("%d %d %d %d\n",receivedFrame->flags,receivedFrame->seq,receivedFrame->crc,receivedFrame->data);
+            printf("%d %d %d %c\n",frameToSend->flags,frameToSend->seq,frameToSend->crc,frameToSend->data);
+            printf("%d %d %d %c\n",receivedFrame->flags,receivedFrame->seq,receivedFrame->crc,receivedFrame->data);
             sendFrame(sfd, frameToSend, *servAddr);
             free(frameToSend);
+            */
         }
     }
 }
